2ded9a4fecef01d53bfa9c4e22ac6bd30aee601f
Weaponeering UI Developer <dev@weaponeering.local>
2026-07-03 15:34:26 +0700

    Revert "Deploy application with GitHub Pages"
    This reverts commit 378eeb43e6f98487714f7bc53d859e5e0d804c1d.

--- vite.config.js
+++ vite.config.js
@@ -6,24 +6,19 @@ const cesiumSource = 'node_modules/cesium/Build/Cesium'
 const cesiumBaseUrl = 'cesium-static'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === 'build' ? '/weaponeering_ui/' : '/'
-
-  return {
-    base,
-    define: {
-      CESIUM_BASE_URL: JSON.stringify(`${base}${cesiumBaseUrl}`),
-    },
-    plugins: [
-      vue(),
-      viteStaticCopy({
-        targets: [
-          { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
-          { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
-          { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
-          { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
-        ],
-      }),
-    ],
-  }
+export default defineConfig({
+  define: {
+    CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
+  },
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
+        { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
+        { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
+        { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
+      ],
+    }),
+  ],
 })

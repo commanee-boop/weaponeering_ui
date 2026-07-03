378eeb43e6f98487714f7bc53d859e5e0d804c1d
Weaponeering UI Developer <dev@weaponeering.local>
2026-07-03 15:29:14 +0700

    Deploy application with GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Analysis from '../views/Analysis.vue'
 import Reports from '../views/Reports.vue'
 
@@ -20,7 +20,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
